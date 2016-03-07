41a86a11f69da8540d12fdf3cacc0490ce91b73c
Paul A. Clarke <pacman@us.ibm.com>
2016-03-07 13:26:44 -0600

    improve and simplify apt distro support
    primarily, retrieve "at" releases instead of hardcoding
    also, use variables for repo URIs instead of multiple hardcodings
    
        Signed-off-by: Paul A. Clarke <pc@us.ibm.com>
    
    Change-Id: Ifc811aabb7b1ac9a1b40dab00d8cfe3260b36080

--- install-sdk.sh
+++ install-sdk.sh
@@ -60,8 +60,10 @@ case "$package_manager" in
 	apt-get)
 		apt-get install software-properties-common # for apt-add-repository
 
+		REPO_URI=ftp://ftp.unicamp.br/pub/linuxpatch/toolchain/at/ubuntu
+
 		# apt-key add 6976a827.gpg.key
-		key="$(download2pipe ftp://ftp.unicamp.br/pub/linuxpatch/toolchain/at/ubuntu/dists/precise/6976a827.gpg.key)"
+		key="$(download2pipe $REPO_URI/dists/trusty/6976a827.gpg.key)"
 		if [ $? -eq 0 ]; then
 			echo "$key" | apt-key add -
 		fi
@@ -69,10 +71,13 @@ case "$package_manager" in
 		if [ "$(uname -p)" = x86_64 ]; then
 			arch=' [arch=i386]'
 		fi
-		apt-add-repository "deb$arch ftp://ftp.unicamp.br/pub/linuxpatch/toolchain/at/ubuntu trusty at9.0"
+		AT_RELEASES="$(download2pipe $REPO_URI/dists/trusty/Release | sed '/Components/s/^Components: \(.*\)$/\1/;tcontinue;d;:continue')"
+		apt-add-repository "deb$arch $REPO_URI trusty $AT_RELEASES"
+
+		REPO_URI=ftp://public.dhe.ibm.com/software/server/iplsdk/v1.8.0/packages/deb/repo
 
 		# apt-key add F20E8D79.gpg.key
-		key="$(download2pipe ftp://public.dhe.ibm.com/software/server/iplsdk/v1.8.0/packages/deb/repo/dists/trusty/F20E8D79.gpg.key)"
+		key="$(download2pipe $REPO_URI/dists/trusty/F20E8D79.gpg.key)"
 		if [ $? -eq 0 ]; then
 			echo "$key" | apt-key add -
 		fi
@@ -80,11 +85,9 @@ case "$package_manager" in
 		if [ "$(uname -p)" = x86_64 ]; then
 			arch=' [arch=amd64]'
 		fi
-		apt-add-repository "deb$arch ftp://public.dhe.ibm.com/software/server/iplsdk/v1.8.0/packages/deb/repo trusty sdk-1.8"
+		apt-add-repository "deb$arch $REPO_URI trusty sdk-1.8"
 
 		apt-get update
-
-		#apt-get install advance-toolchain-at9.0-runtime advance-toolchain-at9.0-devel advance-toolchain-at9.0-perf advance-toolchain-at9.0-mcore-libs
 		;;
 	*)
 		echo "I don't know how to set up your package management system.  Please refer to https://www-304.ibm.com/support/customercare/sas/f/lopdiags/home.html."
